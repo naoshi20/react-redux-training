23a7971c7c323bdc095d8ca97f5b2fe116434d72
naoshi20 <45550449+naoshi20@users.noreply.github.com>
2023-08-20 03:20:46 +0900

    use bindActionCreators

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,53 +1,53 @@
-import {
-    updatePostSuccess,
-    updatePostsFailure,
-    updatePostsStarted,
-    updatePostsSuccess,
-} from "../store/postsSlice";
+// import {
+//     updatePostSuccess,
+//     updatePostsFailure,
+//     updatePostsStarted,
+//     updatePostsSuccess,
+// } from "../store/postsSlice";
 
-export const getPosts = () => {
-    return async (dispatch) => {
-        dispatch(updatePostsStarted());
+// export const getPosts = () => {
+//     return async (dispatch) => {
+//         dispatch(updatePostsStarted());
 
-        await fetch("https://jsonplaceholder.typicode.com/posts")
-            .then(async (res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch");
-                }
-                const data = await res.json();
-                dispatch(updatePostsSuccess(data));
-            })
-            .catch((error) => {
-                dispatch(updatePostsFailure(error.message));
-            });
-    };
-};
+//         await fetch("https://jsonplaceholder.typicode.com/posts")
+//             .then(async (res) => {
+//                 if (!res.ok) {
+//                     throw new Error("Failed to fetch");
+//                 }
+//                 const data = await res.json();
+//                 dispatch(updatePostsSuccess(data));
+//             })
+//             .catch((error) => {
+//                 dispatch(updatePostsFailure(error.message));
+//             });
+//     };
+// };
 
-export const updatePost = () => {
-    return async (dispatch) => {
-        dispatch(updatePostsStarted());
+// export const updatePost = () => {
+//     return async (dispatch) => {
+//         dispatch(updatePostsStarted());
 
-        await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-            method: "PUT",
-            body: JSON.stringify({
-                id: 1,
-                title: "foo",
-                body: "bar",
-                userId: 1,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then(async (res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch");
-                }
-                const data = await res.json();
-                dispatch(updatePostSuccess(data));
-            })
-            .catch((error) => {
-                dispatch(updatePostsFailure(error.message));
-            });
-    };
-};
+//         await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//             method: "PUT",
+//             body: JSON.stringify({
+//                 id: 1,
+//                 title: "foo",
+//                 body: "bar",
+//                 userId: 1,
+//             }),
+//             headers: {
+//                 "Content-type": "application/json; charset=UTF-8",
+//             },
+//         })
+//             .then(async (res) => {
+//                 if (!res.ok) {
+//                     throw new Error("Failed to fetch");
+//                 }
+//                 const data = await res.json();
+//                 dispatch(updatePostSuccess(data));
+//             })
+//             .catch((error) => {
+//                 dispatch(updatePostsFailure(error.message));
+//             });
+//     };
+// };
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,35 +1,96 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getPosts, updatePost } from "../actions/actions";
+import { connect, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+    updatePostSuccess,
+    updatePostsFailure,
+    updatePostsStarted,
+    updatePostsSuccess,
+} from "../store/postsSlice";
 
-const Post = () => {
-    const dispatch = useDispatch();
+const Post = ({
+    updatePostsSuccess,
+    updatePostsFailure,
+    updatePostsStarted,
+    updatePostSuccess,
+}) => {
     const { posts, postsLoading } = useSelector((state) => state.posts);
     console.log(posts);
+
+    const updatePost = () => {
+        updatePostsStarted();
+
+        fetch("https://jsonplaceholder.typicode.com/posts/1", {
+            method: "PUT",
+            body: JSON.stringify({
+                id: 1,
+                title: "foo",
+                body: "bar",
+                userId: 1,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then(async (res) => {
+                if (!res.ok) {
+                    throw new Error("Failed to fetch");
+                }
+                const data = await res.json();
+                updatePostSuccess(data);
+            })
+            .catch((error) => {
+                updatePostsFailure(error.message);
+            });
+    };
 
     useEffect(() => {
         if (postsLoading) {
             return;
         }
 
-        dispatch(getPosts());
+        updatePostsStarted();
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(async (res) => {
+                if (!res.ok) {
+                    throw new Error("Failed to fetch");
+                }
+                const data = await res.json();
+                updatePostsSuccess(data);
+            })
+            .catch((error) => {
+                updatePostsFailure(error.message);
+            });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div>
-            <button onClick={() => dispatch(updatePost())}>button</button>
-            {posts.length < 1 ? (
-                <p>データがありません。</p>
-            ) : (
+            <button onClick={() => updatePost()}>button</button>
+            {posts.length > 0 ? (
                 <ul>
                     {posts.map((post) => (
                         <li key={post.id}>{post.title}</li>
                     ))}
                 </ul>
+            ) : (
+                <p>データがありません。</p>
             )}
         </div>
     );
 };
 
-export default Post;
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(
+        {
+            updatePostsSuccess,
+            updatePostsFailure,
+            updatePostsStarted,
+            updatePostSuccess,
+        },
+        dispatch
+    );
+};
+
+export default connect(null, mapDispatchToProps)(Post);
