62606112f145c55e8ab0834df8bee6ff3cd89456
naoshi20 <45550449+naoshi20@users.noreply.github.com>
2023-08-20 03:22:00 +0900

    Revert "use bindActionCreators"
    This reverts commit 23a7971c7c323bdc095d8ca97f5b2fe116434d72.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,96 +1,35 @@
 import { useEffect } from "react";
-import { connect, useSelector } from "react-redux";
-import { bindActionCreators } from "redux";
-import {
-    updatePostSuccess,
-    updatePostsFailure,
-    updatePostsStarted,
-    updatePostsSuccess,
-} from "../store/postsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts, updatePost } from "../actions/actions";
 
-const Post = ({
-    updatePostsSuccess,
-    updatePostsFailure,
-    updatePostsStarted,
-    updatePostSuccess,
-}) => {
+const Post = () => {
+    const dispatch = useDispatch();
     const { posts, postsLoading } = useSelector((state) => state.posts);
     console.log(posts);
-
-    const updatePost = () => {
-        updatePostsStarted();
-
-        fetch("https://jsonplaceholder.typicode.com/posts/1", {
-            method: "PUT",
-            body: JSON.stringify({
-                id: 1,
-                title: "foo",
-                body: "bar",
-                userId: 1,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then(async (res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch");
-                }
-                const data = await res.json();
-                updatePostSuccess(data);
-            })
-            .catch((error) => {
-                updatePostsFailure(error.message);
-            });
-    };
 
     useEffect(() => {
         if (postsLoading) {
             return;
         }
 
-        updatePostsStarted();
-
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then(async (res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch");
-                }
-                const data = await res.json();
-                updatePostsSuccess(data);
-            })
-            .catch((error) => {
-                updatePostsFailure(error.message);
-            });
+        dispatch(getPosts());
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div>
-            <button onClick={() => updatePost()}>button</button>
-            {posts.length > 0 ? (
+            <button onClick={() => dispatch(updatePost())}>button</button>
+            {posts.length < 1 ? (
+                <p>データがありません。</p>
+            ) : (
                 <ul>
                     {posts.map((post) => (
                         <li key={post.id}>{post.title}</li>
                     ))}
                 </ul>
-            ) : (
-                <p>データがありません。</p>
             )}
         </div>
     );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators(
-        {
-            updatePostsSuccess,
-            updatePostsFailure,
-            updatePostsStarted,
-            updatePostSuccess,
-        },
-        dispatch
-    );
-};
-
-export default connect(null, mapDispatchToProps)(Post);
+export default Post;
